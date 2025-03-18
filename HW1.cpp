559052ceeb37c2f9bb15d12d37a33892c9849e3f
gineslopez11 <glopezpena@udesa.edu.ar>
2025-03-18 10:51:05 -0300

    funciona hasta el dos a)

--- HW1.cpp
+++ HW1.cpp
@@ -1,26 +1,30 @@
 #include <iostream> //se usa para imprimir
 #include <fstream>
 #include <sstream>
+#include <string>
 #include <vector>
 using namespace std; //funcines tipos de datos standard
 
 enum nivelseveridad{
-    DEBUG = 0,
-    INFO = 1,
-    WARNING = 2,
-    ERROR = 3,
-    CRITICAL = 4
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    CRITICAL
 };
 
 vector<vector<int>> matriz_cuadrada (int n);
-void log_message(string mensaje, int severidad);
 
 void imprimir_celdas (const vector<vector<int>>& matriz, int n);
+
+void log_message(string mensaje, enum nivelseveridad severidad);
+
+const char* etiquetar (enum nivelseveridad severidad);
 
 int main(){
     vector<vector<int>> matriz = matriz_cuadrada(3);
     imprimir_celdas (matriz,3);
-    log_message("error", nivelseveridad(3))
+    log_message("hay error", ERROR);
     return 0;
 }
 
@@ -43,15 +47,28 @@ void imprimir_celdas (const vector<vector<int>>& matriz, int n){
         int fila = i / n;      
         int columna = i % n; 
 
-        cout << "M [" + to_string(fila) + "] [" + to_string(columna) + "] = " + to_string(matriz[fila][columna]) << endl;
+        cout << "M [" << fila << "] [" << columna << "] = " << matriz[fila][columna] << endl;
     }
 }
 
-void log_message(string mensaje, int severidad){
+const char* etiquetar (enum nivelseveridad severidad){
+    switch(severidad){
+        case DEBUG: return "DEBUG";
+        case INFO: return "INFO";
+        case WARNING: return "WARNING";
+        case ERROR: return "ERROR";
+        case CRITICAL: return "CRITICAL";
+        default: return "DESCONOCIDO";
+    }
+}
 
+void log_message(string mensaje, enum nivelseveridad severidad){
+
+    const char* etiqueta = etiquetar(severidad);
     ofstream outfile("escribirHW2.txt", ios::app);
     if(outfile.is_open()){
-        outfile<<"["+to_string(severidad)+"] <"+mensaje+"> ";
-    }
+        outfile<<"["<<etiqueta<<"] <"<<mensaje<<"> \n";
+        outfile.close(); }
+
 
 }
